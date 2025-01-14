1f1853267dca869f1ddd130ceeaa2f01b916747c
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2025-01-14 17:25:06 +0700

    change gemini model to flash, add description hovercard for response count indicator

--- components/flash-stats.tsx
+++ components/flash-stats.tsx
@@ -1,13 +1,39 @@
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import * as HoverCard from "@radix-ui/react-hover-card";
 
 type FlashStatsProps = {
   confidentResponses: string[]
   totalQuestions: number
 }
 
+const Indicator = ({ variant = 'green', confidence, responseCount, totalQuestions }: { variant: string, confidence: number, responseCount: number, totalQuestions: number }) => {
+    return (
+        <HoverCard.Root>
+            <HoverCard.Trigger asChild>
+                <div
+                    className={cn("h-full", variant === 'green' ? 'bg-green-400' : variant === 'orange' ? 'bg-orange-400' : '')}
+                    style={{ width: `${variant === 'green' ? confidence : variant === 'orange' ? 100 - confidence : ''}%` }}
+                />
+            </HoverCard.Trigger>
+            <HoverCard.Portal>
+                <HoverCard.Content>
+                    <Card className="mt-4 p-4 bg-muted">
+                        You responded
+                        <strong className={variant === 'green' ? 'text-green-300' : variant === 'orange' ? 'text-orange-300' : ''}>
+                            {variant === 'green' ? ' Got It ' : variant === 'orange' ? ' Study Again ' : ''}
+                        </strong>
+                        on {responseCount} / {totalQuestions} questions
+                    </Card>
+                </HoverCard.Content>
+            </HoverCard.Portal>
+        </HoverCard.Root>
+    )
+}
+
 export default function FlashStats({ confidentResponses, totalQuestions }: FlashStatsProps) {
   const gotItResponse = confidentResponses.reduce((prev, curr) => (curr === 'Got It' ? ++prev : prev), 0);
-  const confidence = (gotItResponse / totalQuestions) * 100;
+  const confidence = Number((gotItResponse / totalQuestions).toFixed(2)) * 100;
 
   return (
     <Card className="w-full">
@@ -18,8 +44,8 @@ export default function FlashStats({ confidentResponses, totalQuestions }: Flash
           </p>
         </div>
         <div className="h-2 w-full border rounded-lg flex overflow-hidden">
-            <div className="bg-green-400 h-full" style={{ width: `${confidence}%` }} />
-            <div className="bg-orange-400 h-full" style={{ width: `${100 - confidence}%` }} />
+            <Indicator variant="green" confidence={confidence} responseCount={gotItResponse} totalQuestions={totalQuestions} />
+            <Indicator variant="orange" confidence={confidence} responseCount={totalQuestions - gotItResponse} totalQuestions={totalQuestions} />
         </div>
       </CardContent>
     </Card>
