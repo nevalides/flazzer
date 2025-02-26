4ae8842ac8689c464c5414b707551e9629b7b21f
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2025-02-26 13:40:04 +0700

    add animation for flash overview page

--- components/flash-overview.tsx
+++ components/flash-overview.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Question } from '@/lib/schemas'
+import { motion } from "motion/react"
 
 type FlashOverviewProps = {
   questions: Question[]
@@ -16,13 +17,13 @@ export default function FlashOverview({ questions, confidentResponse }: FlashOve
           {questions.map((question, questionIndex) => {
             const response = confidentResponse[questionIndex];
             return (
-                <div key={questionIndex} className="mb-8 last:mb-0">
+                <motion.div layout initial={{ y: 20, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} viewport={{ once: true }} transition={{ delay: 0.8 }} key={questionIndex} className="mb-8 last:mb-0">
                 <h3 className="text-lg font-semibold mb-4">{question.question}</h3>
                 <div className="space-y-2 p-4 border-2 rounded-xl">
                     <p>Answer: {question.answer}</p>
                     <p>Your Response: <strong className={response === 'Got It' ? 'text-green-300' : 'text-orange-300'}>{response}</strong></p>
                 </div>
-                </div>
+                </motion.div>
             )
           })}
       </CardContent>
--- components/flash-stats.tsx
+++ components/flash-stats.tsx
@@ -1,46 +1,67 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import * as HoverCard from "@radix-ui/react-hover-card";
+import { animate, motion } from "motion/react";
+import { useEffect, useRef } from "react";
 
 type FlashStatsProps = {
   confidentResponses: string[]
   totalQuestions: number
 }
 
-const Indicator = ({ variant = 'green', confidence, responseCount, totalQuestions }: { variant: string, confidence: number, responseCount: number, totalQuestions: number }) => {
-    return (
-        <HoverCard.Root>
-            <HoverCard.Trigger asChild>
-                <div
-                    className={cn("h-full", variant === 'green' ? 'bg-green-400' : variant === 'orange' ? 'bg-orange-400' : '')}
-                    style={{ width: `${variant === 'green' ? confidence : variant === 'orange' ? 100 - confidence : ''}%` }}
-                />
-            </HoverCard.Trigger>
-            <HoverCard.Portal>
-                <HoverCard.Content>
-                    <Card className="mt-4 p-4 bg-muted">
-                        You responded
-                        <strong className={variant === 'green' ? 'text-green-300' : variant === 'orange' ? 'text-orange-300' : ''}>
-                            {variant === 'green' ? ' Got It ' : variant === 'orange' ? ' Study Again ' : ''}
-                        </strong>
-                        on {responseCount} / {totalQuestions} questions
-                    </Card>
-                </HoverCard.Content>
-            </HoverCard.Portal>
-        </HoverCard.Root>
-    )
+const Indicator = ({ variant = 'green', confidence = 75, responseCount, totalQuestions }: { variant: string, confidence: number, responseCount: number, totalQuestions: number }) => {
+  return (
+      <HoverCard.Root>
+          <HoverCard.Trigger asChild>
+              <motion.div
+                  className={cn("h-full", variant === 'green' ? 'bg-green-400' : variant === 'orange' ? 'bg-orange-400' : '')}
+                  layout
+                  initial={{ width: '50%' }}
+                  animate={variant === 'green' ? { width: `${confidence}%`} : variant === 'orange' ? { width: `${100 - confidence}%`} : {}}
+                  transition={{ delay: 0.8, duration: 1 }}
+              />
+          </HoverCard.Trigger>
+          <HoverCard.Portal>
+              <HoverCard.Content>
+                  <Card className="mt-4 p-4 bg-muted">
+                      You responded
+                      <strong className={variant === 'green' ? 'text-green-300' : variant === 'orange' ? 'text-orange-300' : ''}>
+                          {variant === 'green' ? ' Got It ' : variant === 'orange' ? ' Study Again ' : ''}
+                      </strong>
+                      on {responseCount} / {totalQuestions} questions
+                  </Card>
+              </HoverCard.Content>
+          </HoverCard.Portal>
+      </HoverCard.Root>
+  )
 }
 
 export default function FlashStats({ confidentResponses, totalQuestions }: FlashStatsProps) {
   const gotItResponse = confidentResponses.reduce((prev, curr) => (curr === 'Got It' ? ++prev : prev), 0);
   const confidence = Number((gotItResponse / totalQuestions).toFixed(2)) * 100;
 
+  const confidenceRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const node = confidenceRef.current;
+
+    const controls = animate(0, confidence, {
+      delay: 0.8,
+      duration: 1,
+      onUpdate(value) {
+        if (node) node.textContent = `${value.toFixed(2)}%`;
+      },
+    });
+
+    return () => controls.stop();
+  }, [confidence]);
+
   return (
     <Card className="w-full">
       <CardContent className="space-y-4 p-8">
         <div className="text-center">
           <p className="text-sm text-muted-foreground">
-            I am <br/><span className="text-4xl font-bold text-white">{confidence}%</span><br /> sure you{"\'"}re going to ace this topic
+            I am <br/><span ref={confidenceRef} className="text-4xl font-bold text-white">0%</span><br /> sure you{"\'"}re going to ace this topic
           </p>
         </div>
         <div className="h-2 w-full border rounded-lg flex overflow-hidden">
