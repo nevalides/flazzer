cb987a2e4c533705e4f49868e2f24282296d0dec
Mohammad Hafidh WM <hafidh.wildan@sg-dsa.com>
2025-02-27 17:24:46 +0700

    remove unnecessary comment

--- components/flash-card.tsx
+++ components/flash-card.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 
 type FlashCardProps = {
@@ -12,62 +11,7 @@ type FlashCardProps = {
   
 const FlashCard: React.FC<FlashCardProps> = ({ number, question, answer, hoverAnswer }) => {
     const [showAnswer, setShowAnswer] = useState(false);
-    const MotionCard = motion.create(Card, { forwardMotionProps: true });
     return (
-        // <MotionCard
-        //     onClick={() => setShowAnswer((prev) => !prev)}
-        //     className={cn("flash-card border-0 h-48", showAnswer && 'rotate')}
-        //     style={{ perspective: '1000px' }} // Optional 3D effect
-        //     variants={{
-        //         'Got It': { rotate: 10, x: 30 },
-        //         'Study Again': { rotate: -10, x: -30 },
-        //         'Idle': { rotate: 0, x: 0 }
-        //     }}
-        //     animate={hoverAnswer}
-        //     transition={{ duration: 0.3, easings: ['easeInOut'] }}
-        // >
-        //     <motion.div
-        //         className="flash-card-inner border-2 rounded-xl"
-        //         animate={{ rotateY: showAnswer ? 180 : 0 }}
-        //         transition={{ duration: 0.8 }}
-        //         style={{ transformStyle: 'preserve-3d' }}
-        //     >
-        //         <motion.div
-        //             className="flash-card-front"
-        //             style={{ backfaceVisibility: 'hidden' }}
-        //         >
-        //             <CardHeader className="text-center space-y-2">
-        //                 <CardTitle className="text-xl font-bold rotation">
-        //                     Question {number}
-        //                 </CardTitle>
-        //             </CardHeader>
-        //             <CardContent className="flex justify-center">
-        //                 <p className="text-center text-base">{question}</p>
-        //             </CardContent>
-        //             <CardFooter className="flex justify-center">
-        //                 <p className="text-center text-muted-foreground text-sm">
-        //                     Click card to see the answer
-        //                 </p>
-        //             </CardFooter>
-        //         </motion.div>
-        //         <motion.div
-        //             className="flash-card-back"
-        //             style={{
-        //                 backfaceVisibility: 'hidden',
-        //                 // rotateY: showAnswer ? 0 : 180
-        //             }}
-        //         >
-        //             <CardHeader className="text-center space-y-2">
-        //                 <CardTitle className="text-xl font-bold rotation">
-        //                     Answer
-        //                 </CardTitle>
-        //             </CardHeader>
-        //             <CardContent className="flex justify-center">
-        //                 <p className="text-center text-base">{answer}</p>
-        //             </CardContent>
-        //         </motion.div>
-        //     </motion.div>
-        // </MotionCard>
         <Card
             onClick={() => setShowAnswer((prev) => !prev)}
             className='w-full max-w-md border-0 cursor-pointer h-48'
